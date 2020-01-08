58c38358d568a7d500c590d51822889ecdc15840
djucm <953013236@qq.com>
2020-01-08 22:58:47 +0800

    work12

--- work12/index.js
+++ work12/index.js
@@ -1,8 +1,4 @@
-//获取当前年份
 let year = new Date().getFullYear()
-//调用calendar函数，设置cal内容显示年历
 document.getElementById("cal").innerHTML = calendar(year)
-
-function setCalendar(year) {
-
+function setCalender(year){    
 }
